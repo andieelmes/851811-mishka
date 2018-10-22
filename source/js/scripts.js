d74dd7447bfec1af061ffb55350a399cbef1f045
andieelmes <andie.elmes@gmail.com>
2018-10-23 00:10:38 +0500

    fix js anon functions and add semicolons

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,51 +1,61 @@
 function openPopup(openBtn, modal, inner, className) {
-  var btn = document.querySelectorAll(openBtn)
-  var popup = document.querySelector(modal)
-  var body = document.querySelector('body')
+  var btn = document.querySelectorAll(openBtn);
+  var popup = document.querySelector(modal);
+  var body = document.querySelector('body');
 
   if (!btn.length) return
 
-  btn.forEach(function(element) {
-    element.addEventListener('click', function (e) {
-      e.preventDefault()
-      popup.classList.add(className)
-    })
-  });
+  function open(e) {
+    e.preventDefault();
+    popup.classList.add(className);
+  };
 
-  popup.addEventListener('click', function(e){
+  function close(e) {
+    e.preventDefault();
+    popup.classList.remove(className);
+  };
+
+  function closeByOutsideClick(e) {
     if (!e.target.closest(inner)) {
-      e.preventDefault();
-      popup.classList.remove(className)
+      close(e)
     }
-  })
+  };
 
-  var keyCodeEsc = 27;
-  window.addEventListener('keydown', function (e) {
+  function closeByEsc(e) {
+    var keyCodeEsc = 27;
+
     if (e.keyCode === keyCodeEsc) {
-      e.preventDefault();
-      popup.classList.remove(className)
+      close(e)
     }
+  };
+
+  btn.forEach(function(element) {
+    element.addEventListener('click', open)
   });
+
+  popup.addEventListener('click', closeByOutsideClick)
+
+  window.addEventListener('keydown', closeByEsc);
 
 }
 
-openPopup('.js-cart-btn', '.js-cart-container', '.js-inner', 'active')
+openPopup('.js-cart-btn', '.js-cart-container', '.js-inner', 'active');
 
 
 function toggleMenu() {
   var container = document.querySelector('.js-nav-container');
   var btn = document.querySelector('.js-nav-btn');
 
-  var toOpen = 'Открыть меню'
-  var toClose = 'Закрыть меню'
+  var toOpen = 'Открыть меню';
+  var toClose = 'Закрыть меню';
 
   container.classList.remove('no-js');
 
   btn.addEventListener('click', function() {
-    container.classList.toggle('active')
+    container.classList.toggle('active');
     if (container.classList.contains('active')) btn.setAttribute('aria-label', toClose)
     else btn.setAttribute('aria-label', toOpen)
   });
 }
 
-toggleMenu()
+toggleMenu();
